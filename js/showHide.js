ec8f9b6e8dbfc45fd67ffe83a15ea4a57cf2c7fb
growork <growork@mail.ru>
2019-04-05 23:45:35 +0000

    design for mobile & radio buttons

--- js/showHide.js
+++ js/showHide.js
@@ -4,14 +4,14 @@
           document.getElementById('survey').parentNode.removeChild(survey);
         }
         var obj_show = document.createElement('p');
-        obj_show.innerHTML = "<p id='myst'><input type='number' placeholder='Процент тайника' value='' class='form-element' id='mystery-shopper'></p>";
+        obj_show.innerHTML = "<div id='myst'><p class='input-name'>Процент тайника</p><input type='number' value='' class='form-element' id='mystery-shopper'></div>";
         document.getElementById("mystery-shopper-block").appendChild(obj_show);
       } else {
         if (document.getElementById('myst')){
           document.getElementById('myst').parentNode.removeChild(myst);
         }
         var obj_hide = document.createElement('p');
-        obj_hide.innerHTML = "<p id='survey'><input type='text' placeholder='Коэффициент опроса' value='' class='form-element' id='mystery-shopper'></p>";
+        obj_hide.innerHTML = "<div id='survey'><p class='input-name'>Коэффициент опроса</p><input type='number' value='' class='form-element' id='mystery-shopper'></div>";
         document.getElementById("mystery-shopper-block").appendChild(obj_hide);
       }
     }
