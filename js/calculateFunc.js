f5fd24200ed75d58724136bc6d2168653ec070a1
growork <growork@mail.ru>
2019-11-07 01:03:30 +0300

    refractoring the code in calculateFunc.js

--- js/calculateFunc.js
+++ js/calculateFunc.js
@@ -1,36 +1,34 @@
-    function calculateFunc() {
-      var ratePerHour = parseFloat(document.getElementById("rate-per-hour").value);
-      var hours = parseFloat(document.getElementById("hours").value);
-      var doublePay = parseFloat(document.getElementById("double-pay").value);
-      var planProc = (parseFloat(document.getElementById("plan").value))/100;
-      var mysteryShopper = parseFloat(document.getElementById("mystery-shopper").value);
+function calculateFunc() {
+    var ratePerHour = parseFloat(document.getElementById("rate-per-hour").value);
+    var hours = parseFloat(document.getElementById("hours").value);
+    var doublePay = parseFloat(document.getElementById("double-pay").value);
+    var planProc = (parseFloat(document.getElementById("plan").value)) / 100;
+    var mysteryShopper = parseFloat(document.getElementById("mystery-shopper").value);
 
-      if (document.getElementById('myst')) {
-        mysteryShopper = mysteryShopper/100;
-      }
-if ((mysteryShopper >= 0.6)&&(mysteryShopper < 0.8)) {
-      mysteryShopper = 0.6+(mysteryShopper-0.6)*2;
-} else if (mysteryShopper >= 0.8) {
-mysteryShopper = 1;
-} else {
-mysteryShopper = 0;
-}
-
-      var salary = (hours+doublePay)*ratePerHour;
-      doublePay = doublePay*ratePerHour;
-
-      if ((planProc >= 0.8)&&(planProc <= 1)) {
-        var plan = planProc;
-      } else if (planProc > 1) {
-        var plan = 1+((planProc-1)*2);
-      } else {
-        var plan = 0;
-      }
-      
-      var basePrem = salary * 0.4333;
-
-      var forPay = (salary+(basePrem*plan*0.6 + basePrem*mysteryShopper*0.4))*0.87;
-
-      alert("К выплате: ~"+(parseInt(forPay)));
+    if (document.getElementById('myst')) {
+        mysteryShopper = mysteryShopper / 100;
     }
+
+    if ((mysteryShopper >= 0.6) && (mysteryShopper < 0.8)) {
+        mysteryShopper = 0.6 + (mysteryShopper - 0.6) * 2;
+    } else if (mysteryShopper >= 0.8) {
+        mysteryShopper = 1;
+    } else {
+        mysteryShopper = 0;
+    }
+
+    var salary = (hours + doublePay) * ratePerHour;
+
+    if ((planProc >= 0.8) && (planProc <= 1)) {
+        var plan = planProc;
+    } else if (planProc > 1) {
+        var plan = 1 + ((planProc - 1) * 2);
+    } else {
+        var plan = 0;
+    }
+
+    var basePrem = salary * 0.4333;
+    var forPay = (salary + (basePrem * plan * 0.6 + basePrem * mysteryShopper * 0.4)) * 0.87;
     
+    alert("К выплате: ~" + (parseInt(forPay)));
+}
--- js/showHide.js
+++ js/showHide.js
@@ -1,17 +1,17 @@
-    function showHide(active) {
-      if (active == 'procent') {
-        if (document.getElementById('survey')){
-          document.getElementById('survey').parentNode.removeChild(survey);
+function showHide(active) {
+    if (active == 'procent') {
+        if (document.getElementById('survey')) {
+            document.getElementById('survey').parentNode.removeChild(survey);
         }
         var obj_show = document.createElement('p');
         obj_show.innerHTML = "<div id='myst'><p class='input-name'>Процент тайника</p><input type='number' value='' class='form-element' id='mystery-shopper'></div>";
         document.getElementById("mystery-shopper-block").appendChild(obj_show);
-      } else {
-        if (document.getElementById('myst')){
-          document.getElementById('myst').parentNode.removeChild(myst);
+    } else {
+        if (document.getElementById('myst')) {
+            document.getElementById('myst').parentNode.removeChild(myst);
         }
         var obj_hide = document.createElement('p');
         obj_hide.innerHTML = "<div id='survey'><p class='input-name'>Коэффициент опроса</p><input type='number' value='' class='form-element' id='mystery-shopper'></div>";
         document.getElementById("mystery-shopper-block").appendChild(obj_hide);
-      }
     }
+}
