0e4cc8359c0a6aa33f940644917ebdc2a1634deb
growork <growork@mail.ru>
2019-11-07 14:15:36 +0300

    Added regexp to inputs. Now they may have value from 0 to n including float number. All input type - text

--- js/showHide.js
+++ js/showHide.js
@@ -4,14 +4,14 @@ function showHide(active) {
             document.getElementById('survey').parentNode.removeChild(survey);
         }
         var obj_show = document.createElement('p');
-        obj_show.innerHTML = "<p class='input-name' id='myst'>Процент тайника<input type='number' value='0' class='form-element' id='mystery-shopper'></p>";
+        obj_show.innerHTML = "<p class='input-name' id='myst'>Процент тайника<input type='text' pattern='^(\d+([.,]?\d+)*){1}$' value='' class='form-element' id='mystery-shopper'></p>";
         document.getElementById("mystery-shopper-block").appendChild(obj_show);
     } else {
         if (document.getElementById('myst')) {
             document.getElementById('myst').parentNode.removeChild(myst);
         }
         var obj_hide = document.createElement('p');
-        obj_hide.innerHTML = "<p class='input-name' id='survey'>Коэффициент опроса<input type='number' value='0' class='form-element' id='mystery-shopper'></p>";
+        obj_hide.innerHTML = "<p class='input-name' id='survey'>Коэффициент опроса<input type='text' pattern='^(\d+([.,]?\d+)*){1}$' value='' class='form-element' id='mystery-shopper'></p>";
         document.getElementById("mystery-shopper-block").appendChild(obj_hide);
     }
 }
